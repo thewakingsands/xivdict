227ea9bf3a47523fda9295dcdb36cbea6a088c19
oott123 <git@public.oott123.com>
2021-11-06 23:51:42 +0800

    feat: userscript

---
+++ xivdict.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        FFXIV 划词
+// @require     https://cdn.jsdelivr.net/npm/@thewakingsands/xivdict@0.0.5/dist/xivdict.iife.js
+// @namespace   org.ffcafe.xivdict
+// @match       *://*/*
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 11/6/2021, 11:09:45 PM
+// @inject-into page
+// ==/UserScript==
