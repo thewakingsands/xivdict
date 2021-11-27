7563f04bb1c6bc5650d4675882352354536369c6
oott123 <git@public.oott123.com>
2021-11-27 13:16:18 +0800

    chore: upgrade userscript

--- xivdict.user.js
+++ xivdict.user.js
@@ -1,11 +1,14 @@
 // ==UserScript==
 // @name        FFXIV 划词
 // @require     https://cdn.jsdelivr.net/npm/@thewakingsands/xivdict@latest/dist/xivdict.iife.js
+// @resource    css https://cdn.jsdelivr.net/npm/@thewakingsands/xivdict@latest/dist/style.css
 // @namespace   org.ffcafe.xivdict
 // @match       *://*/*
-// @grant       none
-// @version     1.0
+// @grant       GM_getResourceText
+// @version     1.1
 // @author      -
 // @description 11/6/2021, 11:09:45 PM
 // @inject-into page
 // ==/UserScript==
+
+document.body.appendChild(document.createElement('style')).innerHTML = GM_getResourceText('css')
